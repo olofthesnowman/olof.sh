50ff0d718362f3acd90a7d1db5c8b8b7c7ece09e
Ollie <43708321+olofthesnowman@users.noreply.github.com>
2025-04-12 18:17:27 +0200

    Updated contrast and added metatags.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@
     <h1 class="font-bold text-5xl text-(--ui-primary)">
       olof.sh
     </h1>
-    <small class="text-gray-500 flex items-center gap-2">
+    <small class="text-gray-500 dark:text-gray-400 flex items-center gap-2">
       Deployed with <UIcon name="i-lucide-heart" class="text-red-500" mode="svg" /> of k8s
     </small>
     <UCard variant="outline" class="mt-8 transform-3d hover:translate-z-12 hover:rotate-y-15 hover:-rotate-x-15 hover:rotate-z-10  hover:shadow-2xl transition-all duration-300">
@@ -17,7 +17,7 @@
       <div class="flex gap-4 items-end mt-4">
         <div class="flex flex-col items-end">
           <p class="font-bold text-xl text-(--ui-primary)">"Olof"</p>
-          <p class="text-sm text-gray-400">Worker of IT</p>
+          <p class="text-sm text-gray-500 dark:text-gray-400">Worker of IT</p>
         </div>
         <UAvatar
           src="https://avatars.githubusercontent.com/u/43708321"
@@ -31,3 +31,13 @@
   </div>
   </UContainer>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'olof.sh | IT Worker',
+  ogTitle: 'olof.sh | IT Worker',
+  description: 'Maintainer of unused k8s instances :^)',
+  ogDescription: 'Maintainer of unused k8s instances :^)',
+  twitterCard: 'summary_large_image',
+})
+</script>
